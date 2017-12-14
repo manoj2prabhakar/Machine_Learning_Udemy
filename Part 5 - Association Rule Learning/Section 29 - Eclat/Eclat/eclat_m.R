72941a53992028420b0f129c646b24c10a6b6edf
manoj2prabhakar <manoj2prabhakar@gmail.com>
2017-12-14 08:57:34 +0530

    Reinforcement learning UCB

--- Part 5 - Association Rule Learning/Section 29 - Eclat/Eclat/eclat_m.R
+++ Part 5 - Association Rule Learning/Section 29 - Eclat/Eclat/eclat_m.R
@@ -8,7 +8,7 @@ summary(dataset)
 itemFrequencyPlot(dataset, topN = 10)
 
 # Training Eclat on the dataset
-rules = eclat(data = dataset, parameter = list(support = 0.004))
+rules = eclat(data = dataset, parameter = list(support = 0.004, minlen = 2))
 
 # Visualising the results
-inspect(sort(rules, by = 'lift')[1:10])
+inspect(sort(rules, by = 'support')[1:10])
---
+++ Part 6 - Reinforcement Learning/Section 32 - Upper Confidence Bound (UCB)/UCB/ucb_m.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 14 07:37:22 2017
+
+@author: manoj2prabhakar
+"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+# import dataset
+
+dataset = pd.read_csv('Ads_CTR_Optimisation.csv')
+
+"""#Implementing random selection
+import random
+N = 10000
+d = 10
+ads_selected = []
+total_reward = 0
+for n in range(0, N):
+    ad = random.randrange(d)
+    ads_selected.append(ad)
+    reward = dataset.values[n, ad]
+    total_reward = reward + total_reward
+"""
+
+#implementing the UCB
+import math
+N = 10000
+d = 10
+ads_selected = []
+number_of_selections = [0] * d
+sum_of_rewards = [0] * d
+total_reward = 0
+for n in range(0, N):
+    ad = 0
+    max_upper_bound = 0
+    for i in range(0, d):
+        if(number_of_selections[i]>0):
+            average_reward = sum_of_rewards[i] / number_of_selections[i]
+            delta_i = math.sqrt(3/2 * math.log(n + 1) / number_of_selections[i])
+            upper_bound = average_reward + delta_i
+        else:
+            upper_bound = 1e400
+        if(upper_bound > max_upper_bound):
+            max_upper_bound = upper_bound
+            ad = i
+    ads_selected.append(ad)
+    number_of_selections[ad] = number_of_selections[ad] + 1
+    reward = dataset.values[n, ad]
+    sum_of_rewards[ad] = sum_of_rewards[ad] + reward
+    total_reward = total_reward + reward
+        
+    
+    
+#Visualizing the histogram
+plt.hist(ads_selected)
+plt.title('Histogram of Ads selected')
+plt.xlabel('Ads')
+plt.ylabel('Number of times, the ad is clicked')
+plt.show()
